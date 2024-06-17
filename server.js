f0db0c3a88c81ccb52fdeadd9b74561d88992c9b
Pavel Nazaranka <pvlnzr@gmail.com>
2024-06-17 15:41:13 +0300

    First

---
+++ server.js
@@ -0,0 +1,22 @@
+"use strict";
+
+import express from "express";
+import morgan from "morgan";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(morgan("dev"));
+app.use(express.static('public'));
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.listen(port, ()=>{
+    console.log(`Server running on port: ${port}`);
+});
